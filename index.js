3c6b3ebe50e20b7a4c29d1b51c80d556891b2096
Mohamad Ihsan <ihsan.nutech@gmail.com>
2023-02-16 13:38:24 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const winston = require("./application/helpers/winston.logger");
-const cron = require("node-cron");
-const moment = require("moment");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const winston = require('./application/helpers/winston.logger');
+const cron = require('node-cron');
+const moment = require('moment');
 
-require("dotenv").config();
+require('dotenv').config();
 
-moment.locale("id");
+moment.locale('id');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const app = express();
 
 var corsOptions = {
   origin: [
-    "http://localhost:" + process.env.PORT || 3000,
-    "http://localhost:3000",
+    'http://localhost:' + process.env.PORT || 3000,
+    'http://localhost:3000',
   ],
 };
 
@@ -28,30 +30,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ============================== ROUTES API ==============================
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
-    message: "Welcome to Service Services Card Management System.",
+    message: 'Welcome to Service Services Card Management System.',
   });
 });
 
 app.use((req, res, next) => {
   res.status(404).json({
-      message: 'Ohh you are lost, read the API documentation to find your way back home :)'
-  })
-})
+    message:
+      'Ohh you are lost, read the API documentation to find your way back home :)',
+  });
+});
 
 app.listen(process.env.SERVICE_PORT, () => {
   winston.logger.info(
-    `Server is running on environment: ${process.env.SERVICE_ENV.toUpperCase()} port: ${process.env.SERVICE_PORT}`
+    `Server is running on environment: ${process.env.SERVICE_ENV.toUpperCase()} port: ${
+      process.env.SERVICE_PORT
+    }`
   );
 });
-
 
 // ===================================================
 // SCHEDULER SERVICE
 // ===================================================
-const paymentRequest = require('./application/services/payment-requets')
-const retryPaymentRequest = require('./application/services/payment-requets-retry')
+const paymentRequest = require('./application/services/payment-requets');
+const retryPaymentRequest = require('./application/services/payment-requets-retry');
 
 // ===================================================
 // |      CODE        |          Description
@@ -64,42 +68,42 @@ const retryPaymentRequest = require('./application/services/payment-requets-retr
 // ===================================================
 
 // payment request
-cron.schedule("0 * * * *", async () => { console.log("scheduler service is runnig") })
-cron.schedule("0 2 * * *", async () => { 
+cron.schedule('0 * * * *', async () => {
+  console.log('scheduler service is runnig');
+});
+cron.schedule('0 2 * * *', async () => {
   try {
-    const service = await paymentRequest.service() 
-    console.log(service)
+    const service = await paymentRequest.service();
+    console.log(service);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 // retry
-cron.schedule("30 7 * * *", async () => { 
+cron.schedule('30 7 * * *', async () => {
   try {
-    const service = await retryPaymentRequest.service() 
-    console.log(service)
+    const service = await retryPaymentRequest.service();
+    console.log(service);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-cron.schedule("30 12 * * *", async () => { 
+cron.schedule('30 12 * * *', async () => {
   try {
-    const service = await retryPaymentRequest.service() 
-    console.log(service)
+    const service = await retryPaymentRequest.service();
+    console.log(service);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-cron.schedule("30 17 * * *", async () => { 
+cron.schedule('30 17 * * *', async () => {
   try {
-    const service = await retryPaymentRequest.service() 
-    console.log(service)
+    const service = await retryPaymentRequest.service();
+    console.log(service);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
-
-
+});
